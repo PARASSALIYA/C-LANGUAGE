431c6f9f76f7e4dffa93163d7d343ad645816e09
PARASSALIYA <143994351+PARASSALIYA@users.noreply.github.com>
2023-09-06 15:56:17 +0530

    Add files via upload

---
+++ CH-4/4_1/4-1-2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdio.h>
+main()
+{
+	int a,b;
+	clrscr();
+	printf("Enter A :");
+	scanf("%d",&a);
+	printf("Enter B :");
+	scanf("%d",&b);
+	printf("sum  of %d and %d  ans :%d\n",a,b,a+b);
+	printf("Sub  of %d and %d  ans :%d\n",b,a,b-a);
+	printf("Mult of %d and %d  ans :%d\n",a,b,a*b);
+	printf("Div  of %d and %d  ans :%d\n",a,b,a/b);
+	printf("Modues of %d and %d  ans :%d\n",a,b,a%b);
+	getch();
+}
+
