f37eb0e6ec72b0ab18a72b80f244a7e88fe298c3
PARASSALIYA <parassaliya123@gmail.com>
2023-09-26 17:39:58 +0530

    22/09/2023

---
+++ PR-3/PR-3-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int n,first,last;
+	printf("Enter any number :");
+	scanf("%d",&n);
+	last=n%10;
+	while(n!=0)
+	{
+		n=n/10;
+	}
+	printf("first and last sum :%d",first + last);
+}
