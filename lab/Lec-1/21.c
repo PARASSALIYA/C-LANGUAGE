abc1d65ce332741ab570f147d7f039bf4936b833
PARASSALIYA <parassaliya123@gmail.com>
2023-10-02 13:20:01 +0530

    2/10/2023

---
+++ lab/Lec-1/21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j;
+	 for(i='A';i<='E';i++)
+	 {z
+	 	for(j='A';j<=i;j++)
+	 	{
+	 		printf("%c",j);
+		}
+		printf("\n");
+	 }
+}
---
+++ lab/lab-2/lec-2 (with space)/23.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,s;
+	for(i=1;i<=5;i++)
+	{
+		for(s=1;s<=5-i;s++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			if(j%2==0)
+			{
+				printf("-");
+			}
+			else
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+	}
+}
