bfb5ceb67fdf5a903ee22b36c9164773f3c8be82
PARASSALIYA <143994351+PARASSALIYA@users.noreply.github.com>
2023-10-13 10:34:04 +0530

    Update 8-2-2.c

--- CH-8/8-2/8-2-2.c
+++ CH-8/8-2/8-2-2.c
@@ -2,24 +2,23 @@
 
 main()
 {
-	int y1, y2;
+    int y1, y2;
 	printf("Enter the first number :");
 	scanf("%d", &y1);
 	printf("\nEnter the second number :");
 	scanf("%d", &y2);
-	int a[y2-y1],i,size=0;
+	int a[y2 - y1], i, size = 0;
 	printf("\nThe array is :\n");
 	for (i = y1; i < y2; i++)
 	{
-		if (i % 4 == 0)
-		{
-			a[size]=i;
-			size++;
-		
-		}
+    	if (i % 4 == 0)
+    	{
+        	a[size] = i;
+        	size++;
+    	}
 	}
-	for(i=0;i<size;i++)
+	for (i = 0; i < size; i++)
 	{
-			printf("a[%d]:%d\n",i,a[i]);
+    		printf("a[%d]:%d\n", i, a[i]);
 	}
 }
