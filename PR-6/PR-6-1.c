8667b2b579166931ae0eb144a345331abf9a46b3
PARASSALIYA <parassaliya123@gmail.com>
2023-11-30 22:18:40 +0530

    30/11/2023

--- PR-6/PR-6-1.c
+++ PR-6/PR-6-1.c
@@ -4,13 +4,16 @@ int main()
 {
     char name[5];
     char name1[10];
+    char rev[20];
 
     printf("Enter any string :");
     scanf("%s", &name);
-    strcpy(name1, name);
-    strrev(name);
-    int cmp = strcmp(name, name1);
-    (cmp == 0)
-        ? printf("Given string is a palindrome")
-        : printf("Given string is a not palindrome");
+    for(int i=0;name[i]!=NULL;i++)
+    {
+      const++;
+    }
+    
+    
+     printf("NAME1 :%s",rev[i]);
+
 }
--- PR-6/PR-6-2.c
+++ PR-6/PR-6-2.c
@@ -20,3 +20,4 @@ int main()
         printf("%c : %d\n",n[i],a);
     }
 }
+
